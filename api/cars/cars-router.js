64f1e5962917856b24dd21be2815be3cbeb4f41f
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-05-17 18:47:16 -0400

    Designed DB Scema

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  res.json("I CAN LIVE A BALANCED LYFE");
+})
+
+router.get('/:id', async (req, res, next) => {
+  res.json("I CAN LIVE A BALANCED LYFE");
+})
+
+router.post('/', async (req, res, next) => {
+  res.json("I CAN LIVE A BALANCED LYFE");
+})
+
+module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+  {
+    vin: '1111111111111',
+    make: 'lexus',
+    model: 'dope',
+    mileage: 444,
+    title: 'clean',
+    transmission: 'manual' 
+  },
+  {
+    vin: '1111111111112',
+    make: 'honda',
+    model: 'nice',
+    mileage: 444,
+    title: 'clean',
+  },
+  {
+    vin: '11111111111113',
+    make: 'merc',
+    model: 'fast',
+    mileage: 444, 
+  }
+]
+
+exports.seed = function(knex) {
+  return knex('cars')
+    .truncate().then(() => {
+      return knex('cars').insert(cars)
+    })
+}
